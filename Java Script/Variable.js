541cc36aa7d3798da69b3f516dd8134e465745df
Skanda <skandagowda0@gmail.com>
2025-04-23 20:24:37 +0530

    HTML/CSS

--- Java Script/Variable.js
+++ Java Script/Variable.js
@@ -1,6 +1,18 @@
-var name='skanda'
-var name='sagar'
+var age=10
 let gender='Male'
 gender='Male'
 const institute='X-Workz'
 document.writeln(age)
+
+let emp1={id:14,EmployeeName:'Abhi',salary:45002}
+
+let emp2 = new Object();
+emp2.id=54;
+emp2.EmployeeName='Sharath';
+emp2.salary=47503;
+
+function emp3(id,EmployeeName,salary){
+    this.id=id;
+    this.EmployeeName=EmployeeName;
+    this.salary=salary;
+}
